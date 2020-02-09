42ea9312644af9fc2e18c56d528abb95f64acaa1
James Eapen <jpe4@students.calvin.edu>
2020-02-09 12:34:49 -0500

    fixed config

--- lab01/lab_2.py
+++ lab01/lab_2.py
@@ -3,10 +3,12 @@ from gps import gps
 problem = {
     "start1": ["space on a", "a on b", "b on c", "c on table", "space on table"],
     "finish1": ["space on c", "c on b", "b on a", "a on table", "space on table"],
+
     "start_a": [ "space on table", "b on table", "space on c", "c on a", "a on table"],
     "finish_a": ["b on table", "a on table", "c on table"],
+
     "start_b": ["space on a", "a on table", "space on b", "b on table", "space on c", "c on table"],
-    "finish_b": ["space on a", "a on b", "b on c", "c on table", "space on table"],
+    "finish_b": ["c on table", "b on c", "a on b", "space on a"],
 
     # settings for the Sussman Anamoly, see PAIP, pp. 142. 
     "sussmanStart": ["space on c", "c on a", "a on table", "b on table", "space on table"],
@@ -293,8 +295,8 @@ problem = {
 }
 #
 def main():
-    start = problem['start_a']
-    finish = problem['finish_a']
+    start = problem['start_b']
+    finish = problem['finish_b']
     ops = problem['ops']
     actionSequence = gps(start, finish, ops)
     if actionSequence is None:
