d72c673861f88c66c1e3cbf6fd144327c7691ced
James Eapen <jpe4@students.calvin.edu>
2020-03-08 22:54:22 -0400

    5.4

--- lab05/lab_1.py
+++ lab05/lab_1.py
@@ -11,8 +11,10 @@ It's taken from the AIMA Python code.
 
 import sys
 sys.path.append('/home/james/Documents/Calvin/CS-344/cs344-code/tools/aima')
-from probability import BayesNet, enumeration_ask, elimination_ask, gibbs_ask
+from probability import BayesNet, enumeration_ask, elimination_ask, gibbs_ask, rejection_sampling, likelihood_weighting
 
+
+# Exercise 5.1
 # Utility variables
 T, F = True, False
 
@@ -48,4 +50,23 @@ print(enumeration_ask('Burglary', dict(Alarm=T), burglary).show_approx())
 # Exercise 5.1 aiv.
 print('P(Burglary | john ∧ mary)')
 print(enumeration_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+
+'''Exercise 5.4
+elimination_ask has the same probabilities as enumeration_ask because it is a dynamic version of enumeration_ask.
+Rejection sampling works by using the network created to generate samples. It then removes the samples that don't match the conditions given like the condition that both john and mary call. The final probability is the number of times a burglary occurs in the remaining samples. In this case, it overestimates the probability that the burglary occurs because it rejects the samples that are important in finding a good approximation.
+Liklihood weighting skips the generating and rejection steps by generating only those samples that fit the specified conditions of the case. It gets closer to the exact inference values but is still an overestimate for burglary.
+Gibbs sampling gets the same probabilities as the enumeration_ask and elimination_ask. It works well because the way it transitions between the samples is dependent on those samples' probabilities and allows the sampling to find better estimates that are closer to the actual probabilities.
+'''
+
+print("\nElimination ask")
+print(elimination_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+
+print("Rejection sampling")
+print(rejection_sampling('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+
+print("Liklihood estimate")
+print(likelihood_weighting('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+
+print("Gibbs Sampling ")
+print(gibbs_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
 
